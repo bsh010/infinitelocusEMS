73a1e2c3623a63d3d0e70875c1bd925cdcdbddc1
Bharat Hardi <bharathardi109@gmail.com>
2024-11-30 12:39:54 +0530

    auth backend implemented

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,43 @@
 const express = require("express")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
 
 const app = express()
+app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Hello World")
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "bsbsfbrnsftentwnnwnwn";
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+ 
+    try {
+       const userDoc = await UserModel.create({
+          name,
+          email,
+          password: bcrypt.hashSync(password, bcryptSalt),
+       });
+       res.json(userDoc);
+    } catch (e) {
+       res.status(422).json(e);
+    }
+ });
+ 
+ app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+ 
+    const userDoc = await UserModel.findOne({ email });
+ 
+    if (!userDoc) {
+       return res.status(404).json({ error: "User not found" });
+    }
+ 
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (!passOk) {
+       return res.status(401).json({ error: "Invalid password" });
+    }
 })
-
+ 
 app.listen(3000, ()=> console.log("Server Listening on port 3000"))
